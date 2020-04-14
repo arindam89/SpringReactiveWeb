45af85f25c7ffe57222e5960e8d4038b1731cc6e
Arindam Paul <arindampaul1989@gmail.com>
2020-04-14 19:13:04 +0530

    Added generate and create method

--- src/main/java/com/geekpaul/reactive/intro/BasicFluxMono.java
+++ src/main/java/com/geekpaul/reactive/intro/BasicFluxMono.java
@@ -43,4 +43,76 @@ public class BasicFluxMono {
         flux.concatWith(bang)
                 .subscribe(System.out::println);
     }
+
+    @Test
+    public void FluxWithGenerate() {
+        Flux<String> flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x " + state + " = " + 3*state);
+                    if (state == 10) sink.complete();
+                    return state + 1;
+                });
+        flux.subscribe(System.out::println);
+    }
+
+    /**
+     * create can be very useful to bridge an existing API
+     * with the reactive world - such as an asynchronous API based on listeners.
+     */
+    // Given I have a Async API like the following interface.
+    interface MyEventListener<T> {
+        void onDataChunk(List<T> chunk);
+        void processComplete();
+    }
+
+    @Test
+    public void FluxWithCreate() throws InterruptedException {
+        // Create a Simple Async API
+        class EventProcessor {
+            public MyEventListener l;
+            public void register(MyEventListener listener) {
+                this.l = listener;
+            }
+            public void process() throws InterruptedException {
+                Arrays.asList(1,2,3,4,5)
+                        .forEach(i -> {
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            l.onDataChunk(Arrays.asList(i.toString()));
+                        });
+                Thread.sleep(2000);
+                l.processComplete();
+            }
+        }
+
+        EventProcessor processor = new EventProcessor();
+
+        Flux<String> bridge = Flux.create(sink -> {
+            processor.register(
+                    new MyEventListener<String>() {
+
+                        public void onDataChunk(List<String> chunk) {
+                            for(String s : chunk) {
+                                sink.next(s);
+                            }
+                        }
+
+                        public void processComplete() {
+                            sink.complete();
+                        }
+                    });
+        });
+        bridge.subscribe(
+                System.out::println,
+                null,
+                () -> { System.out.println("Complete Done"); });
+        // Need to trigger else nothing will happen.
+        processor.process();
+    }
+
+
 }
